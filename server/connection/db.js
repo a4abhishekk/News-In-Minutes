a8a37c3ff0fb537e96ab275f438fb8215a8ca0d7
Alok <mishraalok189381@gmail.com>
2024-11-04 21:01:21 +0530

    server done

---
+++ server/connection/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    try {   
+        await mongoose.connect(process.env.MONGO_URL)
+        
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error);
+    }
+}
+
+export default Connection;
